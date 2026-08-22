d906e3dd731f7f08e922310652286624d953b9c7
Apurv <apurvaniket8@gmail.com>
2026-08-22 23:01:28 +0530

    update

---
+++ destinations.js
@@ -0,0 +1,18 @@
+const destinations = [
+{
+ id:"kashmir",
+ name:"Kashmir",
+ region:"North India",
+ budget:"₹22,000",
+ duration:"5-7 Days",
+ image:"assets/kashmir.jpg"
+},
+{
+ id:"goa",
+ name:"Goa",
+ region:"West Coast",
+ budget:"₹15,000",
+ duration:"3-5 Days",
+ image:"assets/goa.jpg"
+}
+];
